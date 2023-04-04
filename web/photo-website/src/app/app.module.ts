034c5710099812f5e7912b444bc55640beee80a1
jbar646 <joe.barry-marron@trademe.co.nz>
2023-04-05 10:08:33 +1200

    creating skeleton components

--- web/photo-website/src/app/app.module.ts
+++ web/photo-website/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { UploadPhotoComponent } from './components/upload-photo/upload-photo.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { PhotoComponent } from './components/photo/photo.component';
+import { LikeComponent } from './components/like/like.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UploadPhotoComponent,
+    UserProfileComponent,
+    PhotoComponent,
+    LikeComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
